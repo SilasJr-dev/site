88149a125fdd0877a0d934fab606a76c7cad62d3
SilasJr-dev <silasvljunior@gmail.com>
2022-10-19 09:31:00 -0300

    att

--- configuração.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    port: '5000',
-    apikeypremium: 'akamepv',
-    limitCount: 5000,
-    dinheiroCount: 10,
-    limitPremium: 99999999,
-    tokens: 'admin_ofc',    
-    dbURI: 'mongodb+srv://TokiBot:<password>@tokibot.p6sqqns.mongodb.net/?retryWrites=true&w=majority' 
-};
---
+++ ideias.js
@@ -0,0 +1,10 @@
+
+//apo´s cadastro dar isso 
+Swal.fire({
+  position: 'center',
+  icon: 'success',
+  title: 'Você foi cadastrado!',
+  showConfirmButton: false,
+  timer: 1500
+})
+
